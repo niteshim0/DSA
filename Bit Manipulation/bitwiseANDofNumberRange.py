a8ca6c91b5bae76c19de2b42e37dd0815efa029d
niteshim0 <niteshim111@gmail.com>
2024-02-21 19:29:31 +0530

    partition dp ques added

--- Bit Manipulation/bitwiseANDofNumberRange.py
+++ Bit Manipulation/bitwiseANDofNumberRange.py
@@ -1,4 +1,6 @@
 class Solution:
+    # do remember n&n-1 always results in mitigation of last set bit
+    # that's the trick here
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         while right>left:
             right = right & (right-1)
