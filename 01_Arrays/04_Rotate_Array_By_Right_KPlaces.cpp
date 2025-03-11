15ee95d7b49990a511543abdfd28fb6086f60238
niteshim0 <niteshim111@gmail.com>
2025-03-11 13:02:31 +0530

    rotate array

---
+++ 01_Arrays/04_Rotate_Array_By_Right_KPlaces.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/rotate-array/description/
+
+/*1.Brute Force : - Take the last k elements in a temp array.
+                  - Move the starting n-k elements to last n-k places.
+                  - Paste the temp elements array into original from start to k places. */
+
+void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        vector<int> temp;
+        for(int i = n-k;i<n;i++){
+            temp.push_back(nums[i]);
+        }
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+        for(int i = 0;i<k;i++){
+            nums[i] = temp[i];
+        }
+}
+
+//Time Complexity : O(n+k)
+//Space Complexity : O(k)
+
+/*2.Optimal Approach : It is based on observation.It can't happen with intuition.*/
+
+class Solution {
+  public:
+      void reverse(vector<int>&nums,int start,int end){
+          int temp;
+          while(start<=end){
+               temp = nums[start];
+               nums[start] = nums[end];
+               nums[end] = temp;
+               start++;
+               end--;
+          }
+      }
+      void rotate(vector<int>& nums, int k) {
+          int n = nums.size();
+          k = k%n;
+          reverse(nums,0,n-k-1);
+          reverse(nums,n-k,n-1);
+          reverse(nums,0,n-1);
+      }
+  };
+
+//Time Complexity : O(N)
+//Space Complexity : O(1)
--- 01_Arrays/questions.md
+++ 01_Arrays/questions.md
@@ -1,3 +1,5 @@
 ## Arrays Problems
 
-- [Find Second Largest Element in Array](https://www.geeksforgeeks.org/problems/second-largest3735/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card)
+- 1. [Find Second Largest Element in Array](https://www.geeksforgeeks.org/problems/second-largest3735/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card)
+- 2. [Remove Duplicates From Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/)
+- 3. [Rotate Array](https://leetcode.com/problems/rotate-array/description/)
