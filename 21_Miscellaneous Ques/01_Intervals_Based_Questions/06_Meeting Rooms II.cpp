3b6702a4c9bd7cef42f6bca62aed6b421aae2744
niteshim0 <niteshim111@gmail.com>
2025-12-17 18:31:36 +0530

    meeting rooms

--- 21_Miscellaneous Ques/01_Intervals_Based_Questions/06_Meeting Rooms II.cpp
+++ 21_Miscellaneous Ques/01_Intervals_Based_Questions/06_Meeting Rooms II.cpp
@@ -1,1 +1,24 @@
 // https://www.geeksforgeeks.org/problems/attend-all-meetings-ii/1
+
+class Solution {
+public:
+    int minMeetingRooms(vector<int> &start, vector<int> &end) {
+        int n = start.size();
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        
+        int rooms = 0;
+        int endPtr = 0;
+
+        for(int i = 0; i < n; i++){
+            if(start[i] < end[endPtr]){
+                rooms++;
+            } else {
+                endPtr++;
+            }
+        }
+        return rooms;
+    }
+};
+// Time Complexity : O(NlogN)
+// Space Complexity : O(N)
