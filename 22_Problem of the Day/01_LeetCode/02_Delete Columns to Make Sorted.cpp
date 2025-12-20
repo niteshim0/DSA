3436efe472fe116292e202684a91087b364f9795
niteshim0 <niteshim111@gmail.com>
2025-12-20 07:58:45 +0530

    LC potd

---
+++ 22_Problem of the Day/01_LeetCode/02_Delete Columns to Make Sorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+         
+         int n = strs.size();
+         int m = strs[0].size();
+
+         char prev = 'A';
+         int cnt = 0;
+         
+         for(int j = 0;j<m;j++){
+            for(int i = 0;i<n;i++){
+                if(prev>strs[i][j]){
+                    cnt++;
+                    break;
+                }
+                prev = strs[i][j];
+            }
+            prev = 'A';
+        }
+
+        return cnt;
+    }
+};
+
+// Time Complexity : O(N*M)
+// Space Complexity : O(1)
