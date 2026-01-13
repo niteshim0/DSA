30f70e0692bc0bb5e6bda11050ced46b5ce842b0
niteshim0 <niteshim111@gmail.com>
2026-01-13 08:46:17 +0530

    gfg potd

---
+++ 22_Problem of the Day/02_GFG/24_Bus Ticket Change.cpp
@@ -0,0 +1,39 @@
+// https://www.geeksforgeeks.org/problems/lemonade-change/1
+// Bus Ticket Change
+// Concept :: Greedy + Nested If-else
+
+class Solution {
+public:
+    bool canServe(vector<int>& bills) {
+        
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            }
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            }
+            else if (bill == 20) {
+            
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
