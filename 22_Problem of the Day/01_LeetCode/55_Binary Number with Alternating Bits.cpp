2c89d5d809493f9c9d4c6c6e2f88230c43dbd491
niteshim0 <niteshim111@gmail.com>
2026-02-18 14:58:19 +0530

    XOR trick

--- 22_Problem of the Day/01_LeetCode/55_Binary Number with Alternating Bits.cpp
+++ 22_Problem of the Day/01_LeetCode/55_Binary Number with Alternating Bits.cpp
@@ -22,3 +22,25 @@ public:
 };
 // TC : log(n) where n = 2^31-1 == O(31) ~ O(1)
 // SC : O(1)
+
+// Technique2 :: Use XOR trick
+// 💡 Key Idea
+// If bits alternate, then:
+// n XOR (n >> 1) will produce a number with all 1s.
+// A number with all 1s satisfies: x & (x + 1) == 0.
+// let say n = 1 0 1 0 1 0 (an alternate bit number)
+// then n>>1 = 0 1 0 1 0 1
+// xor them  = 1 1 1 1 1 1 (all 1's) -- say it is x
+// then x+1 =1 0 0 0 0 0 0 
+// & them   =0 0 0 0 0 0 0 (it should result 0)
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        long long x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+};
+
+// Time Complexity : O(1)
+// Space Complexity : O(1)
