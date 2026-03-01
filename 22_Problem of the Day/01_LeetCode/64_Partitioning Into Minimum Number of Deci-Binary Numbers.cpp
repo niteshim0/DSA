edc658370fabb7f4c2fd2a24f673d65d702851ad
niteshim0 <niteshim111@gmail.com>
2026-03-01 21:12:37 +0530

    dry run

--- 22_Problem of the Day/01_LeetCode/64_Partitioning Into Minimum Number of Deci-Binary Numbers.cpp
+++ 22_Problem of the Day/01_LeetCode/64_Partitioning Into Minimum Number of Deci-Binary Numbers.cpp
@@ -6,6 +6,33 @@
 // Approach :: Think about if the input was only one digit. Then you need to add up as many ones as the value of this digit.If the input has multiple digits, then you can solve for each digit independently, and merge the answers to form numbers that add up to that input.Thus the answer is equal to the max digit.
 // Its pretty simple straightforward ans but thinking it out is hell
 
+// Dry Run
+// Assume max digit in n is x.
+// Because deci-binary only contains 0 and 1,
+// we need at least x numbers to sum up a digit x.
+
+// Now we contruct an answer,
+// Take n = 135 as an example,
+// we initialize 5 deci-binary number with length = 3,
+// a1 = 000
+// a2 = 000
+// a3 = 000
+// a4 = 000
+// a5 = 000
+
+// For the first digit, we fill the first n[0] number with 1
+// For the second digit, we fill the first n[1] number with 1
+// For the third digit, we fill the first n[2] number with 1
+
+// So we have
+// a1 = 111
+// a2 = 011
+// a3 = 011
+// a4 = 001
+// a5 = 001
+
+// Finally, we have 111+11+11+1+1=135.
+
 class Solution {
 public:
     int minPartitions(string n) {
