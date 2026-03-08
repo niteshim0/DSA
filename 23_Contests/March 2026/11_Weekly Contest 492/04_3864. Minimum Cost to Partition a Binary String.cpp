9b81b45e0cce5bedd5c2c82c75a3aa8b0e3ae22f
niteshim0 <niteshim111@gmail.com>
2026-03-08 18:45:00 +0530

    Divide and Conquer

--- 23_Contests/March 2026/11_Weekly Contest 492/04_3864. Minimum Cost to Partition a Binary String.cpp
+++ 23_Contests/March 2026/11_Weekly Contest 492/04_3864. Minimum Cost to Partition a Binary String.cpp
@@ -47,3 +47,41 @@ public:
 // Space Complexity : O(N)
 
 // I should have gone for this ques during contest I would have done much better.
+
+typedef long long ll;
+
+class Solution {
+public:
+    ll enc, flat ;
+    vector<int> pref ;
+    string t ; 
+
+    ll dfs( int l, int r ) {
+        int len = r - l + 1 ; 
+        int ones = pref[r+1] - pref[l] ;
+
+        ll cost ;
+        if( ones == 0) cost = flat ; 
+        else cost = 1LL * len * ones * enc ; 
+
+        if( len % 2 == 0 ) {
+            int mid = ( l + r ) / 2 ; 
+            cost = min( cost, dfs(l, mid ) + dfs( mid + 1, r ) ) ; 
+        }
+
+        return cost; 
+    };
+
+    long long minCost(string s, int encCost, int flatCost) {
+        t = s ; 
+        enc = encCost ;
+        flat = flatCost ; 
+
+        int n = s.size() ;
+
+        pref.assign(n+1, 0 ) ;
+        for( int i = 0 ; i < n ; i++ ) pref[i+1] = pref[i] + ( s[i] == '1' ) ; 
+
+        return dfs(0, n-1 ) ; 
+    }
+};
